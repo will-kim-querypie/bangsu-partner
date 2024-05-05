8e2c18a32233469e97c20b5bd9327c1e8687a3c8
Will Kim <will@chequer.io>
2024-05-06 03:03:56 +0900

    chore: style fix

--- src/shared/ui/gnb/gnb.component.tsx
+++ src/shared/ui/gnb/gnb.component.tsx
@@ -5,8 +5,10 @@ import styles from './gnb.module.css';
 export default function GNB() {
   return (
     <div className={styles.container}>
-      <GNBMobile />
-      <GNBDesktop />
+      <div className={styles.innerContainer}>
+        <GNBMobile />
+        <GNBDesktop />
+      </div>
     </div>
   );
 }
