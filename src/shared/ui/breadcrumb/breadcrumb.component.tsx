f25ae5f21516eeefefffd84047856b0f09da7564
Will Kim <will@chequer.io>
2024-05-06 14:45:00 +0900

    chore: breadcrumb sticky

--- src/shared/ui/breadcrumb/breadcrumb.component.tsx
+++ src/shared/ui/breadcrumb/breadcrumb.component.tsx
@@ -3,15 +3,16 @@
 import { usePathname } from 'next/navigation';
 import BreadcrumbDesktop from './breadcrumb-desktop.component';
 import BreadcrumbMobile from './breadcrumb-mobile.component';
+import styles from './breadcrumb.module.css';
 
 export default function Breadcrumb() {
   const pathname = usePathname();
 
   if (pathname === '/') return null;
   return (
-    <>
+    <div className={styles.container}>
       <BreadcrumbMobile />
       <BreadcrumbDesktop />
-    </>
+    </div>
   );
 }
