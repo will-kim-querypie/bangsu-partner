e4b19cdc05db9297440503c314fc34af47ac01fd
Will Kim <will@chequer.io>
2024-05-07 02:00:11 +0900

    feat: 시공 사진 페이지 리스트

--- src/app/construction-result/[flagship]/page.tsx
+++ src/app/construction-result/[flagship]/page.tsx
@@ -1,5 +1,32 @@
+import type { PostListDto } from '@/app/api/flagship/[flagship]/posts/route';
+import fetcher from '@/shared/api/fetcher';
 import { Flagship } from '@/shared/config/flagship';
+import { Typography } from '@/shared/ui/typography';
+import styles from './page.module.css';
 
-export default function ConstructionResultPage({ params }: { params: { flagship: Flagship } }) {
-  return <main>ConstructionResultPage: {params.flagship}</main>;
+export default async function ConstructionResultPage({ params }: { params: { flagship: Flagship } }) {
+  const { posts }: PostListDto = await fetcher(`/api/flagship/${params.flagship}/posts`);
+
+  return (
+    <main>
+      <ul className={styles.posts}>
+        {posts.map(post => (
+          <li
+            key={post.title}
+            role="button"
+            className={styles.post}
+            style={{
+              background: `url(${post.firstImage.src}) no-repeat center/cover`,
+            }}
+          >
+            <div className={styles.postTitleWrap}>
+              <Typography type="detail1" className={styles.postTitle}>
+                {post.title}
+              </Typography>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
