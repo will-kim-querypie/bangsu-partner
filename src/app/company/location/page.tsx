bda37d03e273d2fec61742cd6563985006386815
Will Kim <will@chequer.io>
2024-05-07 00:59:21 +0900

    feat: 오시는 길 페이지

--- src/app/company/location/page.tsx
+++ src/app/company/location/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import { GeoAltFill } from 'react-bootstrap-icons';
+import { useKakaoLoader, Map, MapMarker } from 'react-kakao-maps-sdk';
+import { ADDRESS, ADDRESS_COORDINATE } from '@/shared/config/company';
+import { Typography } from '@/shared/ui/typography';
+import styles from './page.module.css';
+
+/**
+ * see https://react-kakao-maps-sdk.jaeseokim.dev/docs/setup/withHook/
+ */
 export default function LocationPage() {
-  return <main>LocationPage</main>;
+  useKakaoLoader({
+    id: 'kakao-loader',
+    appkey: process.env.NEXT_PUBLIC_KAKAO_API_KEY as string,
+    libraries: ['services'],
+  });
+
+  return (
+    <main>
+      <Typography type="detail1" as="p" className={styles.text}>
+        <GeoAltFill className={styles.icon} />
+        <span>{ADDRESS}</span>
+      </Typography>
+
+      <Map
+        level={7}
+        center={{ lat: ADDRESS_COORDINATE.latitude, lng: ADDRESS_COORDINATE.longitude }}
+        style={{ width: '100%', height: '360px' }}
+      >
+        <MapMarker position={{ lat: ADDRESS_COORDINATE.latitude, lng: ADDRESS_COORDINATE.longitude }} />
+      </Map>
+    </main>
+  );
 }
--- types/env.d.ts
+++ types/env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_API_HOST?: string;
+      NEXT_PUBLIC_KAKAO_API_KEY?: string;
     }
   }
 }
