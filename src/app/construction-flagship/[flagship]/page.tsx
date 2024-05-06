da616c4d939dc2dacef0bf6db8d8726dba5c417b
Will Kim <will@chequer.io>
2024-05-07 01:29:19 +0900

    feat: 시공 분야 페이지

--- src/app/construction-flagship/[flagship]/page.tsx
+++ src/app/construction-flagship/[flagship]/page.tsx
@@ -1,5 +1,15 @@
-import { Flagship } from '@/shared/config/flagship';
+import { Flagship, FLAGSHIP_DETAILS } from '@/shared/config/flagship';
+import { Typography } from '@/shared/ui/typography';
+import styles from './page.module.css';
 
 export default function FlagShipPage({ params }: { params: { flagship: Flagship } }) {
-  return <main>FlagShipPage: {params.flagship}</main>;
+  const flagShipDetail = FLAGSHIP_DETAILS.find(detail => detail.key === params.flagship);
+
+  return (
+    <main>
+      <Typography type="detail1" className={styles.description}>
+        {flagShipDetail?.description}
+      </Typography>
+    </main>
+  );
 }
