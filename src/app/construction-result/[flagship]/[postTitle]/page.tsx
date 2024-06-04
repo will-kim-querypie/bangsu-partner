0597dcf155ad64acdd02dc63916639243c48feab
Will Kim <will@chequer.io>
2024-06-05 01:49:34 +0900

    refactor: 시공사진 상세 페이지 클라이언트 컴포넌트로 전환

--- src/app/construction-result/[flagship]/[postTitle]/page.tsx
+++ src/app/construction-result/[flagship]/[postTitle]/page.tsx
@@ -1,15 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ArrowLeft } from 'react-bootstrap-icons';
 import { getPostDetail } from '@/shared/api/posts/controller';
 import { Flagship } from '@/shared/config/flagship';
-import BackToListButton from './back-to-list-button.component';
+import { Button } from '@/shared/ui/button';
+import { Typography } from '@/shared/ui/typography';
 import ImageGallery from './image-gallery.component';
 import styles from './page.module.css';
-import Title from './title.component';
 
 export default function ConstructionResultDetailPage({
   params,
 }: {
   params: { flagship: Flagship; postTitle: string };
 }) {
+  const router = useRouter();
+
   const postTitle = decodeURIComponent(params.postTitle);
   const { images } = getPostDetail(params.flagship, postTitle);
   const backUrl = `/construction-result/${params.flagship}`;
@@ -17,12 +23,40 @@ export default function ConstructionResultDetailPage({
   return (
     <main>
       <div className={styles.header}>
-        <Title backUrl={backUrl} content={postTitle} />
+        <Typography className={styles.textTitle} type="title2" overflow="breakWords">
+          {postTitle}
+        </Typography>
+
+        <Button
+          className={styles.buttonTitle}
+          typo="title2"
+          variant="transparent"
+          icon={<ArrowLeft style={{ width: 24, height: 24 }} />}
+          onClick={() => {
+            router.push(backUrl);
+          }}
+          style={{
+            padding: 0,
+          }}
+        >
+          {postTitle}
+        </Button>
       </div>
 
       <ImageGallery images={images} />
 
-      <BackToListButton backUrl={backUrl} />
+      <div className={styles.floatRight}>
+        <Button
+          variant="outline"
+          color="secondary"
+          icon={<ArrowLeft style={{ width: 16, height: 16 }} />}
+          onClick={() => {
+            router.push(backUrl);
+          }}
+        >
+          목록으로 돌아가기
+        </Button>
+      </div>
     </main>
   );
 }
